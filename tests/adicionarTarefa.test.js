052eba5982590e3e50614719be1fc28e2c0126b5
Rafaella Matos <prof.rafaella.matos@gmail.com>
2023-09-28 09:07:03 -0300

    Novos testes unitários no módulo adicionarTarefas

--- tests/adicionarTarefa.test.js
+++ tests/adicionarTarefa.test.js
@@ -11,7 +11,7 @@ describe('Função adicionarTarefa', () => {
         expect(agenda.tarefas).toContain(tarefa);
     });
 
-    test('Deve adicionar várias tarefas à agenda', () => {
+    test('Deve adicionar duas tarefa à agenda', () => {
         const agenda = {
             tarefas: []
         };
@@ -22,6 +22,32 @@ describe('Função adicionarTarefa', () => {
 
         expect(agenda.tarefas).toContain(tarefa1);
         expect(agenda.tarefas).toContain(tarefa2);
+
+        expect(agenda.tarefas.length).toBe(2);
+    });
+
+    test('Deve adicionar várias tarefas à agenda', () => {
+        const agenda = {
+            tarefas: []
+        };
+        const tarefa1 = 'Comprar leite';
+        const tarefa2 = 'Fazer compras';
+        const tarefa3 = 'Estudar';
+        const tarefa4 = 'Trabalhar';
+        const tarefa5 = 'Ir para a academia';
+        adicionarTarefa(agenda, tarefa1);
+        adicionarTarefa(agenda, tarefa2);
+        adicionarTarefa(agenda, tarefa3);
+        adicionarTarefa(agenda, tarefa4);
+        adicionarTarefa(agenda, tarefa5);
+
+        expect(agenda.tarefas).toContain(tarefa1);
+        expect(agenda.tarefas).toContain(tarefa2);
+        expect(agenda.tarefas).toContain(tarefa3);
+        expect(agenda.tarefas).toContain(tarefa4);
+        expect(agenda.tarefas).toContain(tarefa5);
+
+        expect(agenda.tarefas.length).toBe(5);
     });
 
     test('A lista de tarefas deve ter o tamanho correto', () => {
